3838c3b12e95c6c8163b0d56651e0c98090e301c
Bralcorn <bralcrn@asu.edu>
2018-04-05 13:34:10 -0700

    added updating room list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+import RoomList from './components/RoomList.js';
+
+var config = {
+  apiKey: "AIzaSyCeQdlDEfPQUCVnj-YQKJ_65oVHG5K-UxI",
+  authDomain: "bloc-chat-react-c9630.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-c9630.firebaseio.com",
+  projectId: "bloc-chat-react-c9630",
+  storageBucket: "bloc-chat-react-c9630.appspot.com",
+  messagingSenderId: "841302305702"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bloc Chat</h1>
+          <RoomList firebase={firebase}/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rooms:[]
+    }
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+
+
+  }
+  
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({rooms: this.state.rooms.concat( room ) });
+    });
+  }
+
+  render() {
+    return (
+      <div className="room-list">
+        {console.log(this.state.rooms)}
+        {  this.state.rooms.map((room, index) => 
+            <p className="room">Room {room.key}</p>
+          )
+        }
+      </div>
+    )
+  }
+
+}
+
+export default RoomList;
